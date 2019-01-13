74657b1894e6308277ed218e9e22894b8d626007
zzh9504 <zzh9504@outlook.com>
2019-01-13 20:07:54 +0800

    登录 首页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+// 全局样式导入
+import './assets/base.css';
+
+// 导入自己抽取的axios插件
+import VueAxios from './lib/vue-axios';
+Vue.use(VueAxios);
+
+// element-ui导入
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 导入自己抽取的路由
+import router from './lib/router';
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  // 挂载到 最顶级的Vue实例上
+  router,
+  render: h => h(App)
+});
